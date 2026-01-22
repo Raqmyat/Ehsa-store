186dc3c523956c5f18f689a6e5c00edbaa4d8c54
mohamed <elgarhymohamed375@gmail.com>
2026-01-22 12:39:33 +0200

    Create By Eng\Mohamed Ramah Elgarhy 22/1/2026

---
+++ product_location_qty/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Location Quantity',
+    'version': '18.0.1.0.0',
+    'category': 'Inventory',
+    'summary': 'Show product quantity per location like Odoo stock UI',
+    'author': 'Eng/Mohamed Remah Elgarhy / Ritsol Technology',
+    'depends': ['product', 'stock'],
+    'data': [
+        'views/product_template_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
