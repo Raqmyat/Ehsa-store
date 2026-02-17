91b1cbe41c7bfe9c68a035133f0a97d09c8c4175
ritsol <Odoo@ritsol.com>
2026-02-17 15:32:05 +0200

    create By Eng\Mohamed Ramah Elgarhy 17/2/2026

--- salla_product_cost/__manifest__.py
+++ salla_product_cost/__manifest__.py
@@ -22,6 +22,7 @@ Features:
     'depends': ['product', 'stock'],
     'data': [
         'security/ir.model.access.csv',
+        'views/force_standard_price_views.xml',
         'views/product_template_views.xml',
         'views/product_product_views.xml',
         'wizard/fix_salla_product_cost_views.xml',
