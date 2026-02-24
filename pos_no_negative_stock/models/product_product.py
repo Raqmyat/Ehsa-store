258a1507a0a9fa3f35509215b48184a98eb80be4
ritsol <Odoo@ritsol.com>
2026-02-24 09:16:57 +0200

    Update By Eng\Mohamed Ramah Elgarhy 24/2/2026

--- pos_no_negative_stock/models/product_product.py
+++ pos_no_negative_stock/models/product_product.py
@@ -12,18 +12,17 @@ class ProductProduct(models.Model):
             fields.append('free_qty')
         return fields
 
-    def _load_product_with_domain(self, domain, config_id, load_archived=False):
+    def _load_pos_data(self, data):
+        """
+        Injects warehouse into context for stock field computation.
+        """
+        config_id = data['pos.config']['data'][0]['id']
         config = self.env['pos.config'].browse(config_id)
-        context = {
-            **self.env.context,
-            'display_default_code': False,
-            'active_test': not load_archived,
-            'bin_size': True,
-            'warehouse_id': config.warehouse_id.id,
-            'is_storable': True,
-        }
-        return self.with_context(context).search_read(
-            domain,
-            self._load_pos_data_fields(config_id),
-            order='sequence,default_code,name',
-            load=False)
+        warehouse_id = config.picking_type_id.warehouse_id.id
+        
+        self = self.with_context(
+            warehouse_id=warehouse_id,
+            warehouse=warehouse_id,
+        )
+        
+        return super()._load_pos_data(data)
